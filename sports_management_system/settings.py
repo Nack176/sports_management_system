88fbbd43800f256d9650094792802ba506f51fa5
Your Name <you@example.com>
2025-05-31 23:51:57 +0530

    Migrate to PostgreSQL for production deployment

--- sports_management_system/settings.py
+++ sports_management_system/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'sports_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'sports_management_system'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
