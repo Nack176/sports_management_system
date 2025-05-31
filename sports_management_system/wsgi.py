1fc3f38a670cc4db66d616e6554dbdf75e7f8a0d
Your Name <you@example.com>
2025-05-31 23:32:12 +0530

    first commit

--- sports_management_system/wsgi.py
+++ sports_management_system/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sports_management_system.settings')
 
 application = get_wsgi_application()
+
+app = application
+# This is a WSGI application for the sports management system project.
